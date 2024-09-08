de476ae5f81b4b5d671dbb7ee4cfe7896b00475d
cmorellh <c.morell.h@gmail.com>
2024-09-08 10:57:04 -0400

    Update NOAA rt feed and server.js.

--- surf_realtime/server.js
+++ surf_realtime/server.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-const port = 5000; // Port for your proxy server
+const port = 5000;
 
 app.get('/proxy', async (req, res) => {
   try {
     const response = await axios.get('https://www.ndbc.noaa.gov/data/realtime2/41053.spec');
-    res.send(response.data); // Send the response data back to the client
+    res.send(response.data);
   } catch (error) {
     res.status(500).send('Error fetching data');
   }
